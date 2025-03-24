684a8fc8633ea2f1a70fd3f601a75acb0461128f
meet3475 <meetdobariya480@gmail.com>
2025-03-24 14:17:02 +0530

    done changes

--- model/videoModel.js
+++ model/videoModel.js
@@ -4,9 +4,8 @@ const VideoSchema = new mongoose.Schema({
     filePath: { type: String, required: true },
     originalName: { type: String, required: true },
     size: { type: Number, required: true },
-    uploadDate: { type: Date,  default: () => new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })}
-
-})
+    uploadDate: { type: Date, default: Date.now }
+});
 
 const videoModel = mongoose.model('video', VideoSchema);
 module.exports = videoModel;
